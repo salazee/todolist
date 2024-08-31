46bdedd0d16b1309ecd754549a6bdcbe87fb726c
salazee <salawudeeenzainab1@gmail.com>
2024-08-31 15:23:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Todo } from './components/Todo';
+import { TodoForm } from './components/TodoForm';
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+  
+  <TodoWrapper />
+    </div>
+  );
+}
+
+export default App;
